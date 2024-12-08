74086420173b52f4d45992d8e233859a792f8ee5
Soumik Dutta <soumikctg616@gmail.com>
2024-12-08 11:46:51 +0600

    Initial

---
+++ MyStackApp/MyStackApp/MyStack.cs
@@ -0,0 +1,39 @@
+﻿
+namespace MyStackApp
+{
+    public class MyStack
+    {
+        public bool IsEmpty()
+        {
+            return Items.Count == 0;
+        }
+
+        public List<int> Items { get; set; } = new();
+
+        public void Push(int v)
+        {
+            Items.Add(v);
+        }
+
+        public object? Size()
+        {
+            return Items.Count;
+        }
+
+        public int Pop()
+        {
+
+           var item =  Items.Last();
+           Items.Remove(item);
+           return item;
+        }
+
+        #region Extra
+        /*public void DoSomething()
+        {
+            Console.WriteLine("Test Started");
+
+        }*/
+        #endregion
+    }
+}
---
+++ MyStackApp/MyStackAppTest/MyStackTest.cs
@@ -0,0 +1,71 @@
+﻿using MyStackApp;
+using NUnit.Framework;
+
+namespace MyStackAppTest
+{
+    [TestFixture]
+    public class MyStackTest
+    {
+        
+        [Test]
+        public async Task IsEmptyTest()
+        {
+            var stack = new MyStack();
+            Assert.IsTrue(stack.IsEmpty());
+        }
+
+        [Test]
+        public async Task PushOneItem()
+        {
+            var stack = new MyStack();
+            stack.Push(1);
+            Assert.IsFalse(stack.IsEmpty());
+            Assert.AreEqual(1, stack.Size());
+
+        }
+
+        [Test]
+        public async Task PushThreeItem()
+        {
+            var stack = new MyStack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Assert.IsFalse(stack.IsEmpty());
+            Assert.AreEqual(3, stack.Size());
+
+        }
+
+        [Test]
+        public async Task PopOneItem()
+        {
+            var stack = new MyStack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Pop();
+            Assert.IsFalse(stack.IsEmpty());
+            Assert.AreEqual(2, stack.Size());
+
+        }
+
+
+
+        #region extra
+        /*private MyStack _myStack;
+        [SetUp]
+        public void Setup()
+        {
+            _myStack = new MyStack();
+        }
+        [Test]
+        public void DoSomethingTest()
+        {
+            _myStack.DoSomething();
+
+        }*/
+        #endregion
+
+
+    }
+}
